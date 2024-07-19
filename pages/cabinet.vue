c99bb35d0dc9e82375d52bfbc887f93d35981936
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-07-19 13:00:06 +0400

    fix cabinet

--- pages/cabinet.vue
+++ pages/cabinet.vue
@@ -2,13 +2,13 @@
   <main-header v-if="screen === 1" active="2"/>
   <mobile-header v-if="screen === 0" header-name="Личный Кабинет" back-to="/"/>
   <main class="mt-6" v-if="screen === 1">
-
+    1
   </main>
   <main class="mt-6" v-if="screen === 0">
-
+    2
   </main>
   <main class="mt-6" v-else>
-
+    3
   </main>
 </template>
 
